4138bc7bd1736dba0a960c92633f944fa37653d9
Khoa Tran <45887510+ttmkhoa@users.noreply.github.com>
2021-09-24 16:16:37 +0700

    cKhoa upload file java

---
+++ ThreadSimple.java
@@ -0,0 +1,14 @@
+package simple;
+
+public class ThreadSimple extends Thread{
+
+	public void run() {
+		System.out.println("Thread is running...");
+	}
+	public static void main(String[] args) {
+		ThreadSimple t1 = new ThreadSimple();
+		t1.start();
+		
+	}
+
+}
